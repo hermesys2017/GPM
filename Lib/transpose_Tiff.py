cf6b2fbbc676bb84125ab9283e3b947c4940a092
MH.JO <USER@DESKTOP-75M79H8>
2018-11-20 15:49:11 +0900

    Revert "20181120 Transpose tiff 적용(배포 버전 아님)"
    This reverts commit 4748056c0b3df7a182e8628a97876c822242018f.

--- Lib/transpose_Tiff.py
+++ Lib/transpose_Tiff.py
@@ -21,9 +21,6 @@ class transpose_Tiff_class():
         dim_ordering="channels_last"
         dtype='float32'
         file  = gdal.Open(input_tiff)
-        geoTransform = file.GetGeoTransform()
-#         x_min = geoTransform[0]
-#         y_max = geoTransform[3]
         bands = [file.GetRasterBand(i) for i in range(1, file.RasterCount + 1)]
     #    arr = np.array([gdn.BandReadAsArray(band) for band in bands]).astype(dtype)
         arr=numpy.array(file.GetRasterBand(1).ReadAsArray())
@@ -37,25 +34,18 @@ class transpose_Tiff_class():
         DataType = gdal.GDT_Float32 
         prj = osr.SpatialReference()
         prj.ImportFromEPSG(4326) # WGS84
-        pixel_size = 0.1
-        rasterOrigin = (-180,90)
-        origin_X = rasterOrigin[0]
-        origin_Y = rasterOrigin[1]
         #========
         try:
             if dim_ordering=="channels_last":
                 arr = numpy.transpose(arr)  # Reorders dimensions, so that channels are last
                 
                 out_tif = driver.Create(output_tiff, arr.shape[1], arr.shape[0], 1,DataType)
-#                 out_tif.SetGeoTransform((originX, pixelWidth, 0, originY, 0, pixelHeight))
-#                 dataset.SetGeoTransform(( x_min,PIXEL_SIZE,0,y_max, 0, -PIXEL_SIZE))
-                out_tif.SetGeoTransform((origin_X, pixel_size, 0, origin_Y, 0, -pixel_size))
                 out_tif.SetProjection(prj.ExportToWkt())
                 out_band = out_tif.GetRasterBand(1)
                 out_band.WriteArray(arr)
                 out_tif.FlushCache()
                 out_tif = None
-            out_tif.GetGeoTransform()   
+                
         except Exception as e:
             return str(e)
     #         print arr.shape[0],arr.shape[1]
